e30e6e65f3342263995e9d19d91259660d2be603
Vinicius <viniholtman28@gmail.com>
2022-10-16 12:25:05 -0300

    Fixed bug

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,7 +25,7 @@ type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([])
-  const [activerCycleId, setActiverCycleId] = useState<string | null>(null)
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
@@ -36,16 +36,21 @@ export function Home() {
     }
   });
 
-  const activeCycle = cycles.find(cycle => cycle.id === activerCycleId)
+  const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number;
+
     if(activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate)
         )
       }, 1000)
+    }
 
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -58,11 +63,11 @@ export function Home() {
     }
 
     setCycles((state) => [...state, newCycle])
-    setActiverCycleId(newCycle.id)
+    setActiveCycleId(newCycle.id)
+    setAmountSecondsPassed(0)
 
     reset()
   }
-
 
   const totalSeconds = activeCycle ? activeCycle?.minutesAmount * 60 : 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
@@ -72,6 +77,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisabled = !task
